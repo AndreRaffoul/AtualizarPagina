f93e8afcdc9a6b09819fef7f6f628d712c556ecd
Anderson Martins <BlackCode77.1980@gmail.com>
2025-03-10 21:10:24 -0300

    Data 10/03/2025 - Inclusão da rota da HOME

--- AeLModas/src/app/app.routes.ts
+++ AeLModas/src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { CadastroDevedorComponent } from './componentes/devedor/cadastro-devedor/cadastro-devedor.component';
 import { EstoqueComponent } from './componentes/estoque/cadastro/estoque.component';
 import { FornecedoresCadasComponent } from './componentes/fornecedores/cadastro/fornecedores-cadas/fornecedores-cadas.component';
+import { HOMEComponent } from './componentes/HOME/home.component';
 import { CadastroProdutoComponent } from './componentes/produto/cadastro-produto/cadastro-produto.component';
 
 export const routes: Routes = [
 
+  // Path raiz para exibir a home
+  { path: '', component: HOMEComponent },
+
   // Componente FornecedoresCadasComponent
   { path: 'fornecedoresCadas', component: FornecedoresCadasComponent },
-
-  // Rotas de listagem
-  // { path: 'fornecedoresListagem', component: FornecedoresListComponent },
 
   // Componente FornecedoresListComponent
   { path: 'estoqueCadas', component: EstoqueComponent },
