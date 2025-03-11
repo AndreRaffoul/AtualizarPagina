5428c1cce020e881c53360343459829c34480e71
Anderson Martins <BlackCode77.1980@gmail.com>
2025-03-11 10:04:47 -0300

    Data 11/03/2025 - Ajuste de layout na HOME.

--- AeLModas/src/app/componentes/HOME/home.component.ts
+++ AeLModas/src/app/componentes/HOME/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CONTENTComponent } from "./content/content.component";
 
@@ -13,9 +13,21 @@ import { CONTENTComponent } from "./content/content.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HOMEComponent {
+export class HOMEComponent implements AfterViewInit {
 
   imagemLogo: string = 'assets/imagens/logo.png';
   imagemBanner: string = 'banner.jpg';
+
+  constructor() { }
+
+  /* Efeito de animação no nome da loja */
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const nomeLoja = document.querySelector('.nome-loja');
+      if (nomeLoja) {
+        nomeLoja.classList.add('aparecer'); // Ativa a animação
+      }
+    }, 500); // Pequeno delay para suavizar
+  }
 
 }
