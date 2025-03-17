17b5de561d9004b22b9777efbdd4b040a4288ac1
Anderson Martins <BlackCode77.1980@gmail.com>
2025-03-17 20:07:42 -0300

    Data 17/03/2025 - Outras configurações importantes.

--- AeLModas/src/app/app.config.ts
+++ AeLModas/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { routes } from './app.routes';
+
+import { routes } from '../app/app.routes';
+
 import { AuthInterceptorService } from './interceptors/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
